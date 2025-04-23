fda23e82fc3ce3b8780215d234be6d1e835aac3a
morganbyte <morgancacm@gmail.com>
2025-04-23 15:33:02 -0500

    auth gmail + inicio sesión

--- flutter_application_1/lib/data/services/auth_service.dart
+++ flutter_application_1/lib/data/services/auth_service.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
@@ -6,25 +7,47 @@ class Authentication {
   final GoogleSignIn _googleSignIn = GoogleSignIn();
 
   Future<User?> signInWithGoogle() async {
-      try {
-        final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
-        final GoogleSignInAuthentication googleAuth = await googleUser!.authentication;
+    try {
+      final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
+      if (googleUser == null) {
+        return null;
+      }
 
-        // aquí creamos la credencial de autenticación para Firebas
-        final OAuthCredential credential = GoogleAuthProvider.credential(
-          accessToken: googleAuth.accessToken,
-          idToken: googleAuth.idToken,
-        );
+      final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
 
-        // autenticamos al usuario con Firebase
-        final UserCredential userCredential = await _auth.signInWithCredential(credential);
-        return userCredential.user;
+      final OAuthCredential credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth.accessToken,
+        idToken: googleAuth.idToken,
+      );
+
+      final UserCredential userCredential = await _auth.signInWithCredential(credential);
+
+      await _registerUserInFirestore(userCredential.user);
+
+      return userCredential.user;
     } catch (e) {
       print("Error durante el inicio de sesión con Google: $e");
       return null;
-    } 
+    }
   }
 
+  Future<void> _registerUserInFirestore(User? user) async {
+    if (user != null) {
+      final userRef = FirebaseFirestore.instance.collection('users').doc(user.uid);
+
+      final doc = await userRef.get();
+      if (!doc.exists) {
+        await userRef.set({
+          'uid': user.uid,
+          'email': user.email,
+          'displayName': user.displayName ?? 'Sin nombre',
+          'createdAt': FieldValue.serverTimestamp(),
+        });
+      }
+    }
+  }
+
+  // Función para cerrar sesión
   Future<void> signOut() async {
     await _googleSignIn.signOut();
     await _auth.signOut();
